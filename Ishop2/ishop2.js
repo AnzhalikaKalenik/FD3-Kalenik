949711d2fd05f8a6003577352bcfe4f48eab73db
Anzhelika Kalenik <anzhalikaremez@mail.ru>
2022-07-16 03:53:21 +0300

    modification

--- Ishop2/ishop2.js
+++ Ishop2/ishop2.js
@@ -8,7 +8,7 @@ var Ishop2 = React.createClass ({
             React.PropTypes.shape({
               product: React.PropTypes.string.isRequired,
               code: React.PropTypes.number.isRequired,
-              prise: React.PropTypes.number.isRequired,
+              price: React.PropTypes.number.isRequired,
               photo: React.PropTypes.string.isRequired,
               balance: React.PropTypes.number.isRequired,
               control: React.PropTypes.string.isRequired,
@@ -18,7 +18,7 @@ var Ishop2 = React.createClass ({
 
     getInitialState: function (){  //магазин должен знать какой товар выделен
       return{
-        selectedItemCode: this.props.newSelectedItemCode,
+        selectedItemCode: null,
         itemsList:this.props.startItemsList//текущий itemsList,где начальное значение startItemsList
       };
     },
@@ -38,28 +38,33 @@ var Ishop2 = React.createClass ({
         )
     },
 
-    render: function (){ // и выглядит компонент вот так 
+    render: function (){ 
       var startItemsListCode=this.props.startItemsList.map( v =>
-        React.createElement(Item, {key:v.code, product:v.product, price:v.price, photo:v.photo, balance:v.balance, control:v.control })
-      );
+        React.createElement(Item, {key:v.code, 
+          product:v.product, price:v.price, photo:v.photo, balance:v.balance, control:v.control,
+          cbSelected:this.state.selected,
+          cbDelete:this.deleteItem}),
+       );
 
           // var startItemsListCode = [];
           // for ( var a=0; a<this.props.startItemsList.length; a++ ) {//проходим циклом повсем вариантам списка товаров
           //     var startItemList=this.props.startItemsList[a];
-          //     var startItemListCode=        
-          //       React.DOM.div({key:startItemList.code,className:'StartItemList'},// у каждого элемента массива должен быть свой уникальный кеу
-          //         React.DOM.span({className:'Product'},startItemList.product),
-          //         React.DOM.span({className:'Price'},startItemList.price),
-          //         React.DOM.span({className:'Photo'},startItemList.photo, React.DOM.img({src:productList.photo})),
-          //         React.DOM.span({className:'Balance'},startItemList.balance),
-          //         React.DOM.span({className:'Control'},startItemList.control, React.DOM.input({type:button, value:productList.control})),
-          //       );
-          //     startItemsListCode.push(startItemListCode);
-          //   }
+
+              // var startItemListCode=        
+              //   React.DOM.div({key:startItemList.code,className:'StartItemList'},// у каждого элемента массива должен быть свой уникальный кеу
+              //     React.DOM.span({className:'Product'},startItemList.product),
+              //     React.DOM.span({className:'Price'},startItemList.price),
+              //     React.DOM.span({className:'Photo'},startItemList.photo, React.DOM.img({src:productList.photo})),
+              //     React.DOM.span({className:'Balance'},startItemList.balance),
+              //     React.DOM.span({className:'Control'},startItemList.control, React.DOM.input({type:button, value:productList.control})),
+              //   );
+              // startItemsListCode.push(startItemListCode)
+      
 
           return React.DOM.div( {className:'Ishop2'}, 
             React.DOM.div( {className:'startItemsList'}, startItemsListCode),//startItemsListCode массив описанный выше
-          );
+          )
       
     }
 })
+    
--- Ishop2/item.js
+++ Ishop2/item.js
@@ -16,7 +16,7 @@ var Item = React.createClass ({
     },
 
 
-    clicked: function(){ //она знает что на нее кликнули,и должна сказать магазину,что она теперь должна быть выделена    
+    clicked: function(EO){ //она знает что на нее кликнули,и должна сказать магазину,что она теперь должна быть выделена    
         this.props.cbSelected(this.props.code); //когда мы по товару щелкнули он,вызывает this.props.cbSelected 
         //фактически функцию selected и сообщает какой товар выделить.
         //чтобы узнать какой товар выделен используем (this.props.code)
@@ -25,8 +25,8 @@ var Item = React.createClass ({
 
 
     delete: function(EO){ //своя функция delete вызывает props cbDelete и передает ему this.props.code 
-        this.props.cbDelete(this.props.code);
-        eo.stopPropagation();     
+        this.props.cbDelete(this.props.code); //(this.props.code);
+        EO.stopPropagation();     
     } ,
     //т.е. когда на товаре щелкаем удалить(button),вызывается функция delete
     //она вызывает cbDelete и передает ему код товара this.props.code
@@ -43,14 +43,57 @@ var Item = React.createClass ({
 
     render: function(){
 
+        if (this.props.selectedItemCode=this.props.code){
+            return React.DOM.div( null,
+                React.DOM.label({className:'blockItem'},
 
+                    React.DOM.input({type:'button', 
+                        value:this.props.code, 
+                        name:'buttonItem',
+                        onClick:this.clicked,
+                    }),
 
-        return React.DOM.div( {className:'Item'}, 
+                    React.DOM.span({
+                        style:{color:(this.props.selectedItemCode==this.props.code)?'orange':'black'}
+                    }, this.props.product),
+
+                    React.DOM.span({
+                        style:{color:(this.props.selectedItemCode==this.props.code)?'orange':'black'}
+                    }, this.props.price),
+
+                    React.DOM.span({
+                        style:{color:(this.props.selectedItemCode==this.props.code)?'orange':'black'}
+                    },this.props.photo),
+
+                    React.DOM.span({
+                        style:{color:(this.props.selectedItemCode==this.props.code)?'orange':'black'}
+                    },this.props.balance),
+
+                    React.DOM.span({
+                        style:{color:(this.props.selectedItemCode==this.props.code)?'orange':'black'}
+                    },this.props.control),
+        
+                )
+            )
+        }
+
+        else{
+           return React.DOM.div( {className:'Item'}, 
             React.DOM.span({className:'Product'},this.props.product),
             React.DOM.span({className:'Price'},this.props.price),
             React.DOM.span({className:'Photo'},this.props.photo, React.DOM.img({src:this.props.photo})),
             React.DOM.span({className:'Balance'},this.props.balance),
-            React.DOM.span({className:'Control'},this.props.control, React.DOM.input({type:button, value:this.props.control})),
+            React.DOM.span({className:'Control'},this.props.control, React.DOM.input({type:'button', value:this.props.control})),
           );
+        }  
     }
 });
+// 0. Есть Массив с товарами "startItemsListArr", одно из свойств товара - уникальный code.
+// 1. Есть компонент со списком товаров "ishop2". Получает props с массивом товаров. Рендерит для каждого товара компонент
+//    "item".В state у "ishop2" есть selectedProductCode - код выбранного (подсвеченного) сейчас товара, изначально - null.
+// 2. Компонент "item".Получает props с товаром(не надо отдельными реквизитами, сразу весь товар)и props с признаком, выделена ли строка.
+//    Рендерит строку товара со светлым фоном (не выделена) или с тёмным (выделена).
+// 3. По клику на строку товара надо его выделить. А то есть,"ishop2" передаёт всем "item" ещё один пропс - коллбек cbSelectProduct. 
+//    Когда мы щёлкаем по строке с товаром,"item" знает что он щёлкнут и он вызывает этот коллбек, сообщая "ishop2", что теперь выделен 
+//    именно он. "ishop2" меняет свой стейт selectedProductCode на переданный коллбеку, и т.к. стейт изменился, вся таблица товаров 
+//    перерисовывается уже с новым выделенным товаром.
