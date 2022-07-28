0baf3e2a66783b766b2eb08e3b5aaa3302b107ea
Anzhelika Kalenik <anzhalikaremez@mail.ru>
2022-07-29 00:59:30 +0300

    rainbow frame

---
+++ RainbowFrame/App.js
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import RainbowFrame from './components/RainbowFrame';
+
+let colorsArr=require('./colors.json');
+
+ReactDOM.render(
+  <RainbowFrame 
+    colors={colorsArr}
+  />
+  , document.getElementById('container') 
+);
+
---
+++ RainbowFrame/components/RainbowFrame.js
@@ -0,0 +1,29 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './RainbowFrame.css';
+
+// import VotesQuestion from './VotesQuestion';
+// import VotesAnswer from './VotesAnswer';
+
+// import colorsArr from './colors.json';
+var colors = require('./colors.json');
+
+class RainbowFrame extends React.Component {
+
+  static propTypes = {
+    colors:PropTypes.arrayOf.isRequired,
+  };
+
+  render() {
+
+    let curr = this.props.children;
+    for (let color of this.props.colors){
+      curr = <div style={{border:"solid 1px"+color}}>{curr}</div>
+    }
+
+    return curr;
+  }
+}
+
+export default RainbowFrame;
